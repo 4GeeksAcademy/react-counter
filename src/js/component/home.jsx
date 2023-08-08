2183148015b511bcad213ae14e0848840e943a27
Matias Borba <85084252+19Mega@users.noreply.github.com>
2023-08-08 17:40:31 +0000

    my fake clock

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,22 @@
 import React from "react";
+import Decenas from "./decenas";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+console.log("hola")
+
+const Home = ({unidad, decenas, centenas, mil, diezMil, cienMil}) => {
+
+    return (
+        <div className="text-center">
+			<div className="row align-items-center">
+
+			<Decenas unidad={unidad} decenas={decenas} centenas={centenas} mil={mil} diezMil={diezMil} cienMil={cienMil}/>
+			</div>
+        </div>
+    );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,47 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+
+let num = 0;
+let decenas = 0;
+let centenas = 0;
+let mil = 0;
+let diezMil=0;
+let cienMil = 0;
+
+function displayHello() {
+    //render your react application
+    //            qué vamos a dibujar,    donde lo vamos a dibujar
+    ReactDOM.render(<Home unidad={num} decenas={decenas} centenas={centenas} mil={mil} diezMil={diezMil} cienMil={cienMil}/>, document.querySelector("#app"));
+    num++;
+
+    if (num>2) {
+        decenas ++
+        num=0
+    }
+
+    if (decenas>2) {
+        centenas ++
+        decenas=0
+    }
+
+    if (centenas>2) {
+        mil ++
+        centenas=0
+    }
+
+    if (mil>2) {
+        diezMil++
+        mil=0
+    }
+
+    if (diezMil>2) {
+        cienMil++
+        diezMil=0
+    }
+}
+
+setInterval(displayHello, 1000);
